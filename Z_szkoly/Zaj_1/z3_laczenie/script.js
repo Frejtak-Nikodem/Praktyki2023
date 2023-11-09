4fd473656696423167a6452f667674d4fb055878
Frejtak-Nikodem <89976269+Frejtak-Nikodem@users.noreply.github.com>
2023-11-09 18:25:49 +0100

    Add files via upload

---
+++ Z_szkoly/Zaj_1/z3_laczenie/script.js
@@ -0,0 +1,8 @@
+
+function Laczenie() {
+    const arr1 = [1,2];
+    const arr2 = [3,4];
+    const arr3= arr1 + "," + arr2;
+
+    console.log(arr3);
+}
---
+++ Z_szkoly/Zaj_1/z5_suma/script.js
@@ -0,0 +1,7 @@
+const tab = [4,5,11];
+let wynik = 0;
+
+for(let i=0; i<tab.length; i++) {
+    wynik += tab[i];
+}
+console.log(wynik)

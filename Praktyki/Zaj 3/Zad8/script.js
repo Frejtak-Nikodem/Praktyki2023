f1b8efa4bb66df4e7feed850546719ff112f541e
Frejtak-Nikodem <89976269+Frejtak-Nikodem@users.noreply.github.com>
2023-11-21 17:51:04 +0100

    Dodane komentarze

--- Praktyki/Zaj 3/Zad8/script.js
+++ Praktyki/Zaj 3/Zad8/script.js
@@ -1,3 +1,4 @@
+// Definicja tablicy obiektów reprezentujących książki
 const books = [
     {title: 'Total loss 100', pages: 600, genre: 'fantasy', rating: 7},
     {title: 'Total enlightenment', pages: 250, genre: 'romance', rating: 4},
@@ -8,10 +9,17 @@ const books = [
     {title: 'Paranormal', pages: 200, genre: 'thriller', rating: 5},
 ];
 
-const sortBooksDesc = (list) => list.sort((a, b) => b.title.length - a.title.length);
-const mapToTitle = (list) => list.map(({title}) => title);
+// Funkcja sortDown sortuje książki malejąco według długości tytułu
+const sortDown = (list) => list.sort((a, b) => b.title.length - a.title.length);
 
+// Funkcja mapTit mapuje książki na ich tytuły
+const mapTit = (list) => list.map(({title}) => title);
+
+// Funkcja compose łączy kilka funkcji w jedną, wykonując je w kolejności od prawej do lewej
 const compose = (...fns) => (x) => fns.reduce((acc, fn) => fn(acc), x);
 
-let result = compose(sortBooksDesc, mapToTitle);
+// Wywołanie funkcji compose z funkcjami sortDown i mapTit
+let result = compose(sortDown, mapTit);
+
+// Wyświetlenie drugiego elementu wyniku (drugiego najdłuższego tytułu)
 console.log(result(books)[1]);

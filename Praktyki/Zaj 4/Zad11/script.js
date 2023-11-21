82f5017f799ff6664606a685801f766e45e6170a
Frejtak-Nikodem <89976269+Frejtak-Nikodem@users.noreply.github.com>
2023-11-21 18:00:08 +0100

    Dodane komentarze

--- Praktyki/Zaj 4/Zad11/script.js
+++ Praktyki/Zaj 4/Zad11/script.js
@@ -1,11 +1,13 @@
+// Funkcja sprawdzająca stan odpowiedzi
 const check = (response) => {
     if (response.status == 200) {
-        return response.json(); // Zwraca w formacie JSON
+        return response.json(); // Zwraca dane w formacie JSON
     } else {
-        throw new Error('Błąd w pobieraniu danych'); // Jeżeli status się nie zgadza, error, który jest
+        throw new Error('Błąd w pobieraniu danych'); // Rzuca błąd, jeśli status odpowiedzi nie wynosi 200
     }
 };
 
+// Funkcja do wyświetlania danych JSON w konsoli
 const print = (dataJSON) => {
     for (const post of dataJSON) {
         console.log('Title:', post.title);
@@ -14,12 +16,14 @@ const print = (dataJSON) => {
     }
 };
 
-fetch('https://jsonplaceholder.typicode.com/posts') //bez error
+// Pobieranie danych z pierwszego URL
+fetch('https://jsonplaceholder.typicode.com/posts') // Bez błędu
     .then(check)
     .then(print)
     .catch(error => console.error('Wystąpił błąd:', error));
 
-fetch('https://jsonplaceholder.typicode.com/posts2') //Z error
+// Pobieranie danych z drugiego URL
+fetch('https://jsonplaceholder.typicode.com/posts2') // Z błędem (np. brak zasobu)
     .then(check)
     .then(print)
     .catch(error => console.error('Wystąpił błąd:', error));

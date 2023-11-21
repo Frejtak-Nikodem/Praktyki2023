9132ae2861f7b4f5cff5e177f0ab2c4f61e5410f
Frejtak-Nikodem <89976269+Frejtak-Nikodem@users.noreply.github.com>
2023-11-21 17:40:41 +0100

    Dodane komentarze

--- Praktyki/Zaj 3/Zad6/script.js
+++ Praktyki/Zaj 3/Zad6/script.js
@@ -1,3 +1,4 @@
+// Definicja tablicy obiektów reprezentujących książki
 const books = [
     {title: 'Total loss 100', pages: 600, genre: 'fantasy', rating: 7},
     {title: 'Total enlightenment', pages: 250, genre: 'romance', rating: 4},
@@ -7,11 +8,15 @@ const books = [
     {title: 'World Ender 3', pages: 199, genre: 'fantasy', rating: 1},
     {title: 'Paranormal', pages: 200, genre: 'thriller', rating: 5},
 ];
-
+// Funkcja filter przyjmuje listę książek i filtruje tylko te, które spełniają określone warunki
 const filter = (list) => list.filter((book) => book.genre.endsWith("y") && book.pages%2 == 0);
+
+// Funkcja sum przyjmuje listę książek i zwraca sumę liczby liter w tytułach książek (bez spacji)
 const sum = (book) => book.reduce((totalLetters, book) => totalLetters + book.title.replace(/\s/g, '').length, 0);
 
+// Funkcja compose łączy kilka funkcji w jedną, wykonując je w kolejności od prawej do lewej
 const compose = (...fns) => (x) => fns.reduce((acc, fn) => fn(acc), x);
 
+// Wywołanie funkcji compose z funkcjami filter i sum oraz przekazanie tablicy books jako argumentu
 let result = compose(filter, sum);
 console.log(result(books));

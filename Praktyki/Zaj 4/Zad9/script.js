147e26eec4757db7a69d43d9ce76061077f72a63
Frejtak-Nikodem <89976269+Frejtak-Nikodem@users.noreply.github.com>
2023-11-21 17:57:38 +0100

    Dodane komentarze

--- Praktyki/Zaj 4/Zad9/script.js
+++ Praktyki/Zaj 4/Zad9/script.js
@@ -1,6 +1,9 @@
+// Tworzenie obietnicy za pomocą obiektu Promise
 const myPromise = new Promise((resolve) => {
+    // Ustawienie timera, który rozwiąże obietnicę po 5 sekundach
     setTimeout(() => resolve("Udało się"), 5000);
 })
 
+// Obsługa rezultatu obietnicy po jej rozwiązaniu za pomocą metody then
 myPromise
 .then(console.log);

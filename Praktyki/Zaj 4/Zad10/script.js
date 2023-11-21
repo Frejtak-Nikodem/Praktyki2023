28e73fc40b7d97e9734e989a3039bad1e25205e6
Frejtak-Nikodem <89976269+Frejtak-Nikodem@users.noreply.github.com>
2023-11-21 17:55:57 +0100

    Dodane komentarze

--- Praktyki/Zaj 4/Zad10/script.js
+++ Praktyki/Zaj 4/Zad10/script.js
@@ -1,21 +1,28 @@
-function multiplyAsync(x, y){
+// Funkcja multiplyAsync przyjmuje dwa argumenty x i y, i zwraca obiekt Promise
+function multiplyAsync(x, y) {
+    // Tworzenie obiektu Promise z funkcją wykonawczą, która sprawdza, czy x i y są liczbami
     return myPromise = new Promise((resolve, reject) => {
-        if(typeof(x) == 'number' && typeof(y) == 'number'){
+        if(typeof(x) == 'number' && typeof(y) == 'number') {
+            // Jeśli oba argumenty są liczbami, rozwiązuj Promise wynikiem mnożenia
             resolve(x*y);
-        }else{
+        }
+        else {
+            // Jeśli przynajmniej jeden z argumentów nie jest liczbą, odrzuć Promise z odpowiednim komunikatem błędu
             reject("Niepoprawne argumenty funkcji!!!");
         }
     });
 }
 
-multiplyAsync(2,5)
-.then((result) =>{
+// Wywołanie funkcji multiplyAsync z poprawnymi argumentami (liczbami)
+multiplyAsync(3,6)
+.then((result) => {
     console.log("Rezultat mnożenia: "+result);
 })
 .catch(console.error);
 
-multiplyAsync(2,'5')
-.then((result) =>{
+// Wywołanie funkcji multiplyAsync z niepoprawnymi argumentami (co najmniej jeden z argumentów nie jest liczbą)
+multiplyAsync(3,'6')
+.then((result) => {
     console.log("Rezultat mnożenia: "+result);
 })
 .catch(console.error);
